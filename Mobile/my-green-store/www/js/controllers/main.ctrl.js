227c69af24311b6c558693246e520c8056407a96
Wellerson Miranda <weller.miranda@gmail.com>
2016-05-21 17:07:14 -0400

    login

--- Mobile/my-green-store/www/js/controllers/main.ctrl.js
+++ Mobile/my-green-store/www/js/controllers/main.ctrl.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  function MainController($scope, $ionicModal, $timeout) {
+  function MainController(apiUrl, $scope, $ionicModal, $timeout, $cordovaOauth, $http) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -32,23 +32,46 @@
 
     // Perform the login action when the user submits the login form
     $scope.doLogin = function () {
-      $cordovaOauth.google("781562583420-jp5dnffk58hu8qa37jjb59mvnauaks2r.apps.googleusercontent.com", ["email"])
-        .then(function(result) {
-          $scope.loginMessage = '';
-          $scope.loginData = user;
 
-          console.log(user);
-          console.log('Doing login', $scope.loginData);
+      function onError(error, a, b) {
+        console.log(error);
+        console.log(a);
+        console.log(b);
+        $scope.loginMessage = 'Login error, please try again.'
+      };
+
+      function googleOAuth(callback) {
+        $cordovaOauth.google("781562583420-jp5dnffk58hu8qa37jjb59mvnauaks2r.apps.googleusercontent.com", ["email"])
+          .then(callback, function(error) { console.log('googleOAuth!'); return onError(error); });
+      }
+
+      function login(token, callback) {
+        $http.get(apiUrl + '/login', { headers: { token: token }})
+          .then(callback, function(error) { console.log('login!'); return onError(error); });
+      }
+
+      googleOAuth(function(result) {
+        console.log('googleOAuth: ', result);
+
+        login(result.access_token, function(response) {
+          console.log('login response', response);
+
+          if(!response.data.success)
+            return onError();
+
+          $scope.loginMessage = '';
+          $scope.loginData = response.data.user.token;
+
+          // TODO: save token in localstorage
+          console.log(response);
 
           $scope.closeLogin();
-        }, function(error) {
-          console.log(error);
-          $scope.loginMessage = 'Login error, please try again.'
         });
+      });
     };
   }
 
   angular.module('mgstore')
-    .controller('MainController', ['$scope', '$ionicModal', '$timeout', MainController]);
+    .controller('MainController', ['apiUrl', '$scope', '$ionicModal', '$timeout', '$cordovaOauth', '$http', MainController]);
 
 } ());
